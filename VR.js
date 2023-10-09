315ba0292a8f8971f6b4c143db117d21d6c60f22
Danyang Li <dyang.l@outlook.com>
2023-10-09 23:45:24 +0100

    newpage

---
+++ VR.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const videoContainer = document.querySelector('.video_container');
+    const videospheres = document.querySelectorAll('a-videosphere');
+    const playPauseBtn = document.getElementById('playPause');
+    const volumeSlider = document.getElementById('volume');
+    const fullscreenBtn = document.getElementById('fullscreen');
+    const closeButton = document.getElementById('close');
+    const progress = document.getElementById('progress');
+    const currentTimeElem = document.getElementById('currentTime');
+    const totalTimeElem = document.getElementById('totalTime');
+
+    let activeVideo = null;
+
+    function formatTime(seconds) {
+        const mins = Math.floor(seconds / 60);
+        const secs = Math.floor(seconds % 60);
+        return `${mins}:${String(secs).padStart(2, '0')}`;
+    }
+
+    document.querySelectorAll('.btn').forEach(button => {
+        button.addEventListener('click', function () {
+            const videosphereId = this.dataset.videosphereId;
+            videospheres.forEach(vs => {
+                if (vs.id === videosphereId) {
+                    vs.setAttribute('visible', 'true');
+                    activeVideo = vs.components.material.material.map.image;
+                    activeVideo.addEventListener('timeupdate', function () {
+                        currentTimeElem.textContent = formatTime(activeVideo.currentTime);
+                        progress.value = (activeVideo.currentTime / activeVideo.duration) * 100;
+                    });
+                } else {
+                    vs.setAttribute('visible', 'false');
+                }
+            });
+            videoContainer.style.visibility = 'visible';
+            activeVideo.currentTime = 0;
+            currentTimeElem.textContent = formatTime(0);
+            totalTimeElem.textContent = formatTime(activeVideo.duration);
+            progress.value = 0;
+        });
+    });
+
+    playPauseBtn.addEventListener('click', function () {
+        if (activeVideo.paused) {
+            activeVideo.play();
+            this.innerHTML = '<i class="fa-regular fa-circle-pause"></i>';
+        } else {
+            activeVideo.pause();
+            this.innerHTML = '<i class="fa-regular fa-circle-play"></i>';
+        }
+    });
+
+    volumeSlider.addEventListener('input', function () {
+        if (activeVideo) {
+            activeVideo.volume = this.value;
+        }
+    });
+
+    fullscreenBtn.addEventListener('click', function () {
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+        } else {
+            videoContainer.requestFullscreen();
+        }
+    });
+
+    closeButton.addEventListener('click', function () {
+        if (!activeVideo.paused) {
+            activeVideo.pause();
+            playPauseBtn.innerHTML = '<i class="fa-regular fa-circle-play"></i>';
+        }
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+        }
+        videoContainer.style.visibility = 'hidden';
+    });
+
+    progress.addEventListener('input', function () {
+        const seekTime = (this.value / 100) * activeVideo.duration;
+        activeVideo.currentTime = seekTime;
+    });
+});
