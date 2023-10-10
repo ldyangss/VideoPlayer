724f84d9d7c4dadc03c704c4e386745716352a73
Danyang Li <dyang.l@outlook.com>
2023-10-10 15:54:45 +0100

    changes

--- VR.js
+++ VR.js
@@ -8,6 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const progress = document.getElementById('progress');
     const currentTimeElem = document.getElementById('currentTime');
     const totalTimeElem = document.getElementById('totalTime');
+    const ap2btn = document.getElementById('ap2btn');
+    const ap2container = document.querySelector('.ap2container');
+    const ap2close = document.getElementById('ap2close');
+    const ap2video = document.querySelector('.ap2container video');
 
     let activeVideo = null;
 
@@ -79,4 +83,30 @@ document.addEventListener('DOMContentLoaded', function () {
         const seekTime = (this.value / 100) * activeVideo.duration;
         activeVideo.currentTime = seekTime;
     });
+
+    // ap2 video
+    ap2btn.addEventListener("click", function() {
+        console.log("ap2btn clicked");  // 添加此行
+        ap2container.style.visibility = "visible";
+        ap2video.pause();
+    });
+    
+    // 关闭视频窗口并返回到主页面
+    ap2close.addEventListener("click", function() {
+        ap2container.style.visibility = "hidden";
+        ap2video.currentTime = 0;
+    
+        // 如果视频在全屏模式下，退出全屏
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+        }
+    });
+
+
+
 });
+
+
+
+   
+
